21af76f6f5699ad0199f86b71cbd8c19972c50a2
Pianma <ikkw2040@naver.com>
2022-05-25 22:37:07 +0900

    spring study

---
+++ hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -0,0 +1,17 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    //회원이 저장소에 저장
+    Member save(Member member);
+    //findbyid로 가져오는데 null일때 그냥 반환하지 않고 Optional로 감싸서 반환
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+
+    //지금까지 저장된 모든 회원리스트를 반환
+    List<Member> findAll();
+}
---
+++ hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,43 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements  MemberRepository {
+
+    //메모리라서 저장을 하기 위해 Map을 씀
+    private  static Map<Long, Member> store = new HashMap<>();
+    //시퀀스는 0,1,2 이렇게 키 값을 생성해 주는 놈
+    private  static long sequence = 0L;
+
+
+    @Override
+    public Member save(Member member) {
+        //id셋팅
+        member.setId(++sequence);
+        //스토어에 저장-map에 저장됨
+        store.put(member.getId(), member);
+        return  member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        //그냥 store.get(id)를 한다면 id가 없을경우 null이 된다. 옛날에는 이렇게 썻음
+        //하지만 요새는 Optional로 감싸서 반환한다.
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        //getName이 파라메터 name과 같은지 확인
+        //같은 경우에만 필터링이 된다
+        //findAny()->하나라도 찾는다.
+       return store.values().stream().filter(member -> member.getName().equals(name)).findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
