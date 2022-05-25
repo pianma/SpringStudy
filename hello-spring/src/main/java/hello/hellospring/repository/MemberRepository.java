88894d8d4e720f5911e1081135ed06e48e6bed38
Pianma <ikkw2040@naver.com>
2022-05-26 01:12:48 +0900

    회원가입

--- hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -6,8 +6,10 @@ import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository {
+
     //회원이 저장소에 저장
     Member save(Member member);
+
     //findbyid로 가져오는데 null일때 그냥 반환하지 않고 Optional로 감싸서 반환
     Optional<Member> findById(Long id);
     Optional<Member> findByName(String name);
---
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,29 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    //회원가입
+    public Long join(Member member){
+        //같은 이름이 있는 중복 회원x
+        validateDuplicateMember(member);
+
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        Optional<Member> result = memberRepository.findByName(member.getName());
+        result.ifPresent(m ->{
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+}
